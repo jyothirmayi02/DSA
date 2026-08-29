21486b76afed3ccc9d81a06b1e2afd1ad6cfcaa2
jyothirmayi02 <jyothirmayib2006@gmail.com>
2026-08-30 00:21:41 +0530

    Time: 1 ms (100%), Space: 44 MB (40.12%) - LeetHub

--- 4006-count-valid-prefixes/4006-count-valid-prefixes.java
+++ 4006-count-valid-prefixes/4006-count-valid-prefixes.java
@@ -1,21 +1,10 @@
 class Solution {
     public int countValidPrefixes(String s) {
-        int n = s.length();
-        int[] count_0 = new int[n];
-        int[] count_1 = new int[n];
-        if (s.charAt(0) == '0') count_0[0] = 1;
-        else count_1[0] = 1;
-        int ans = 1;
-        for (int i = 1; i < n; i++){
-            if (s.charAt(i) == '0'){
-                count_0[i] = count_0[i-1] + 1;
-                count_1[i] = count_1[i-1];
-            }
-            else {
-                count_1[i] = count_1[i-1] + 1;
-                count_0[i] = count_0[i-1];
-            }
-            if (Math.abs(count_0[i] - count_1[i]) == 1 || Math.abs(count_0[i] - count_1[i]) == 0)
+        int ans = 0, c0 = 0, c1 = 0;
+        for (char ch : s.toCharArray()){
+            if (ch == '0') c0++;
+            else c1++;
+            if (Math.abs(c0 - c1) <= 1)
                 ans++;
         }
         return ans;
